5391c77eab00214696a47fb76d66539ff85c3aeb
Aditya <adityamhetre10@gmail.com>
2025-11-28 11:45:52 +0530

    railway file changed

--- src/main/java/com/example/makemytrip/railway.java
+++ src/main/java/com/example/makemytrip/railway.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class railway {
     @GetMapping("/railway")
-    public String getData() {return  "Please book railways ticket from booking.com at 10% discount" ; }
+    public String getData() {return  "Please book railways ticket from booking.com at 50% discount" ; }
 }
