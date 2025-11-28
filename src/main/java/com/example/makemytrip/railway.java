14075b785cfbbf74d9a7d469010309508aaf82bb
Aditya <adityamhetre10@gmail.com>
2025-11-28 12:29:23 +0530

    Changes

--- src/main/java/com/example/makemytrip/railway.java
+++ src/main/java/com/example/makemytrip/railway.java
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class railway {
     @GetMapping("/railway")
-    public String getData() {return  "Please book railways ticket from booking.com at 30% discount" ; }
+    public String getData() {return  "Please book railways ticket from local at 30% discount" ; }
+    public String getData() {return  "Please book railways ticket from local1 at 30% discount" ; }
 }
