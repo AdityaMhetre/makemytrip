14dd7985a0536345316fc88541d9bc1d06b22230
Aditya <adityamhetre10@gmail.com>
2025-11-28 10:54:05 +0530

    Changes in flight and railway file

--- src/main/java/com/example/makemytrip/flight.java
+++ src/main/java/com/example/makemytrip/flight.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class railways {
-    @GetMapping("/railways")
-    public String getData() {return  "Please book railways ticket from makemytrip at 10% discount" ; }
+public class flight {
+    @GetMapping("/flight")
+    public String getData() {return  "Please book Aeroplane ticket from makemytrip at 20% discount" ; }
 }
--- src/main/java/com/example/makemytrip/railway.java
+++ src/main/java/com/example/makemytrip/railway.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Flight {
-    @GetMapping("/flight")
-    public String getData() {return  "Please book Aeroplane ticket from makemytrip at 20% discount" ; }
+public class railway {
+    @GetMapping("/railway")
+    public String getData() {return  "Please book railways ticket from makemytrip at 10% discount" ; }
 }
