ce8c1c97de4d071147db1890d4cb9aca172f7aae
Aditya <adityamhetre10@gmail.com>
2025-12-13 17:25:06 +0530

    changes in jenkins file

--- src/main/java/com/example/makemytrip/flight.java
+++ src/main/java/com/example/makemytrip/flight.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class flight {
     @GetMapping("/flight")
-    public String getData() {return  "Please book Aeroplane ticket from makemytrip at 20% discount including bonus" ; }
+    public String getData() {return  "Please book Aeroplane ticket from go ibibio at 20% discount including bonus" ; }
 }
